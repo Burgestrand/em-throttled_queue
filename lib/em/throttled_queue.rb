b3770a95dad8f8a792caa01b7f60be2bf809e37a
Kim Burgestrand <kim@burgestrand.se>
2011-02-13 00:56:29 +0100

    Convert to YARDoc documentation

--- lib/em/throttled_queue.rb
+++ lib/em/throttled_queue.rb
@@ -1,18 +1,16 @@
+# coding: utf-8
 require 'eventmachine'
 
+# @see https://github.com/eventmachine/eventmachine
 module EventMachine
   # An EM::Queue with a rate-limit applied to it. This is useful if you
   # wish to consume items at a limited pace.
   # 
   # @todo Add a maximum size-limit on amount of items in queue.
-  # @todo Allow specifying how many ticks per second.
   class ThrottledQueue < Queue
-    # Public: Create a new rate-limited queue.
+    # Create a new rate-limited queue.
     # 
-    # limit - Numeric maximum of dequeues every tick
-    # delay - Numeric delay in seconds between ticks (default: 1)
-    # 
-    # Examples
+    # @example
     # 
     #   EM::ThrottledQueue.new(0.5)
     #   # => dequeues 0.5 times every second (1 time every 2 seconds)
@@ -20,7 +18,8 @@ module EventMachine
     #   EM::ThrottledQueue.new(10, 0.2)
     #   # => dequeues 10 times every 0.2 seconds
     # 
-    # Returns nothing.
+    # @param [Numeric] limit maximum of dequeues every “tick”
+    # @param [Numeric] delay number of seconds between ticks (default: 1)
     def initialize(limit = 0, delay = 1)
       @limit   = limit
       @credits = 0
@@ -32,28 +31,30 @@ module EventMachine
       super()
     end
     
-    # Public: Pop an item off the queue as soon as conditions allow. The
-    # given block is executed in the reactor thread.
+    # Pop an item off the queue as soon as conditions allow, passing it
+    # to the given block.
     #
-    # Returns self.
-    def pop(*a, &b)
-      cb = EM::Callback(*a, &b)
-      interact { @popq.push(cb) }
+    # @yield [item] callback to execute when item is popped off
+    # @yieldparam item an item from the queue
+    # @return (see #interact)
+    # @see http://rdoc.info/github/eventmachine/eventmachine/master/EventMachine.Callback
+    def pop(&block)
+      interact { @popq.push(block) }
     end
     
-    # Public: Push items onto the queue on the next reactor tick.
+    # Push items onto the queue (from within the reactor thread) asap.
     # 
-    # *items - any number of items to be pushed onto the queue.
-    # 
-    # Returns self.
+    # @param [item, …] items items to be pushed onto the queue
+    # @return (see #interact)
     def push(*items)
       interact { @items.push(*items) }
     end
     
     private
       # Helper method to schedule queue interaction and then _dequeue.
-      #
-      # Returns self.
+      # 
+      # @yield block to call within EM::schedule
+      # @return [ThrottledQueue]
       def interact(&block)
         EM::schedule do
           block.call
@@ -65,49 +66,13 @@ module EventMachine
       
       # Dequeue as many items as ossible.
       # 
-      # Returns nothing.
+      # @return [ThrottledQueue]
       def scheduled_dequeue
         until @credits < 1 || @items.empty? || @popq.empty?
           @credits -= 1
           @popq.shift.call @items.shift
         end
+        self
       end
-  end
-end
-
-if $0 == __FILE__ then
-  
-  ticks = 0
-  deqs  = 0
-  n     = 0
-  
-  puts "Running EM for 5 seconds..."
-  EM::run do
-    EM::add_timer(5) { EM::stop } # run maximum 5 seconds (50 jobs)
-    queue = EM::ThrottledQueue.new(1, 0.1)
-    queue.push(*(0..1000).to_a)
-    
-    ticker = proc do |me|
-      ticks += 1
-      queue.pop { |i| deqs += 1 and n += i }
-      EM::next_tick { me.call(me) }
-    end
-    
-    ticker.call(ticker)
-  end
-  
-  # TODO:
-  # - test for #pop order (FIFO)
-  # - test for #pop rate limit
-  # - test for #pop arguments 
-  # - report EM::schedule possible lock-up bug (see #interact)
-  
-  puts "N: #{n}"
-  print "With #{ticks} pops (ticks), I dequeued #{deqs} times in 5 seconds: "
-  
-  if (40..50).include?(deqs) and ticks > 100
-    puts "this is OK!"
-  else
-    abort "this is a bug!"
   end
 end
