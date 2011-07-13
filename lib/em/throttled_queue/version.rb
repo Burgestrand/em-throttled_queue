f246b6276a5d622959348554f250ba05b37a38bb
Kim Burgestrand <kim@burgestrand.se>
2011-07-13 12:38:05 +0200

    Bump to version 1.1.0

--- lib/em/throttled_queue/version.rb
+++ lib/em/throttled_queue/version.rb
@@ -15,6 +15,6 @@ module EventMachine
   class ThrottledQueue < Queue
     # Gem version, following Semantic Versioning since v1.0.2
     # @see http://semver.org/
-    VERSION = [1, 0, 3].join('.')
+    VERSION = [1, 1, 0].join('.')
   end
 end
